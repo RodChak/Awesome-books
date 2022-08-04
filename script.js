3486315b37cab72f8a8daa50665fd10752fe1440
RodChak <97179349+RodChak@users.noreply.github.com>
2022-08-04 15:00:37 +0200

    add script file
    Signed-off-by: RodChak <97179349+RodChak@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,52 @@
+let booksContainer = document.getElementById('books');
+let inpTitle = document.getElementById('inp-title');
+let inpAuthor = document.getElementById('inp-author');
+let bookDatas = [];
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+//When the remove button is clicked
+function removeBook(i) {
+  bookDatas.splice(i, 1);
+  generateNewBook();
+}
+
+// function for generating and displaying new book using dynamic html
+function generateNewBook(i) {
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < bookDatas.length; i += 1) {
+  booksContainer.innerHTML += `<div class="book" id="book">
+                     <p class="the-title">${bookDatas[i].title} by ${bookDatas[i].author}</p>
+                     <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
+                     <hr><br></div>`;
+}
+}
+
+// once you click Button new object is created: following function takes place: 
+// pushes the new object into array bookDatas
+// the new array Bookdatas is saved in the local storage
+const addBtn = document.getElementById('add-btn');
+addBtn.addEventListener('click', () => {
+  const newBook = new Book(inpTitle.value, inpAuthor.value);
+  bookDatas.push(newBook);
+  generateNewBook(bookDatas.length - 1);
+  localStorage.setItem('bookDatas', JSON.stringify(bookDatas));
+});
+removeBook();
+
+// the load event is fired when the whole webpage (HTML) has loaded fully,
+// loading books after refreshing the page
+// The getItem() method returns the current value associated with the given key. 
+window.addEventListener('load', () => {
+  let loadedData = JSON.parse(localStorage.getItem('bookDatas'));
+  if (loadedData) {
+    generateNewBook();
+  } else {
+    loadedData = [];
+  }
+});
